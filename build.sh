a84c0db3621c5765d5897d64127feeca29bae009
vidwath <vidwathrhebse@gmail.com>
2019-05-14 10:06:33 +0530

    code coverage modified

--- build.sh
+++ build.sh
@@ -1,7 +1,12 @@
 #!/bin/sh
 cd Code/Test
-python test_for_features.py -v — with-coverage
-python test_for_scrapetwitter.py -v — with-coverage
-python test_for_spacy.py -v — with-coverage
-python test_for_tesseract.py -v — with-coverage
-python test_for_webscrapping.py -v — with-coverage
+python test_for_features.py 
+python test_for_scrapetwitter.py 
+python test_for_spacy.py 
+python test_for_tesseract.py 
+python test_for_webscrapping.py 
+coverage run test_for_features.py 
+coverage run test_for_scrapetwitter.py
+coverage run test_for_spacy.py
+coverage run test_for_tesseract.py 
+coverage run test_for_webscrapping.py 
