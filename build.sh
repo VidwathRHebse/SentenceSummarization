65530f8a7f3db3a08db11cb13228af280d9ccf47
vidwath <vidwathrhebse@gmail.com>
2019-05-15 10:45:59 +0530

    code coverage

--- build.sh
+++ build.sh
@@ -6,7 +6,7 @@ cd Code/Test
 #python test_for_tesseract.py 
 #python test_for_webscrapping.py 
 coverage run test_for_features.py 
-#coverage run test_for_scrapetwitter.py
-#coverage run test_for_spacy.py
-#coverage run test_for_tesseract.py 
-#coverage run test_for_webscrapping.py 
+coverage run test_for_scrapetwitter.py
+coverage run test_for_spacy.py
+coverage run test_for_tesseract.py 
+coverage run test_for_webscrapping.py 
